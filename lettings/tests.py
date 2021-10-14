5d7594a4c69563fb19180edb7bc30e2eed923e8e
Corentin BRAVO <corentinbravo@gmail.com>
2021-10-14 11:16:43 +0200

    changed stuff

--- lettings/tests.py
+++ lettings/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from lettings.models import Address, Letting
+
+
+class LettingsTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.address = Address(
+            number=5,
+            street="Advance Street",
+            city="Helena",
+            state="MT",
+            zip_code="33058",
+            country_iso_code="USA"
+        )
+        cls.address.save()
+        cls.letting = Letting(
+            title="Letting of Montana",
+            address=cls.address
+        )
+        cls.letting.save()
+
+    def test_title_of_the_index_page_is_correct(self):
+        response = self.client.get(reverse('lettings_index'))
+        self.assertContains(response, '<title>Lettings</title>')
+
+    def test_title_of_a_letting_page_is_correct(self):
+        response = self.client.get(reverse('letting', kwargs={"letting_id": self.letting.pk}))
+        self.assertContains(response, f'<title>{self.letting.title}</title>')
+
+
+
