b9f1abd2910fd5ae697b0f52410210694e6adaf8
Corentin BRAVO <corentinbravo@gmail.com>
2021-10-19 10:22:36 +0200

    fixed dockerfile/circleCI

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,7 +1,7 @@
 import os
+from dotenv import dotenv_values
 
-config = os.environ
-# config = dotenv_values(".env")
+config = dotenv_values(".env")
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
