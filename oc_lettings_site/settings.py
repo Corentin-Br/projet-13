26689d2c445452835c45e08c1cd6b7cbc8159da7
Corentin BRAVO <corentinbravo@gmail.com>
2021-10-20 13:43:26 +0200

    fixed typo

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,8 +1,8 @@
 import os
 from dotenv import dotenv_values
 
-
-config = dotenv_values(".env")
+config = os.environ
+# config = dotenv_values(".env")
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
