90cd64211a1f08e153af55b41e67ceaf9a8c01d3
Corentin BRAVO <corentinbravo@gmail.com>
2021-10-21 10:15:08 +0200

    linting

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -16,7 +16,6 @@ if config.get('ENV', "development") == 'production':
     )
 
 
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
